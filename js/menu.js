32a1dd71526b3584c0daee1d21c12e18ef7121ad
turov <ilya.turov@gmail.com>
2024-03-29 12:59:38 +0300

    update

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,11 @@
 const button = document.querySelector('.header__menu-btn');
+const companies = document.querySelector('.header__companies');
+const companiesList = document.querySelector('.header__partners-list');
 
 button.onclick = function () {
     button.classList.toggle('active');
+}
+
+companies.onclick = function () {
+    companiesList.classList.toggle('active');
 }
